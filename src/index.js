88bd96310e9c68894f6b6c47d59ce8a6597de9f8
SelvinMP <selvinmartinez378@gmail.com>
2026-03-25 18:57:55 -0600

    Configurado CORS para Firebase

--- src/index.js
+++ src/index.js
@@ -17,9 +17,16 @@ const app = express();
 
 // 1. Middlewares de Seguridad y Control
 app.use(helmet()); // Protege de ataques web comunes
-app.use(cors());   // Permite que tu App de React se conecte sin bloqueos
+//app.use(cors());   // Permite que tu App de React se conecte sin bloqueos
 app.use(morgan('dev')); // Muestra en consola qué rutas se están visitando
 app.use(express.json()); // Permite recibir datos en formato JSON (como el Login)
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://gv-security.web.app', // Tu URL de Firebase
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 // 2. Conexión a la Base de Datos (GV-Security)
 connectDB();
@@ -52,8 +59,8 @@ app.post('/api/check-connection', (req, res) => {
 
 // 4. Configuración del Puerto
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log('==============================================');
-    console.log(`🚀 Servidor corriendo en: http://localhost:${PORT}`);
+    console.log(`🚀 Servidor corriendo en puerto: ${PORT} (Accesible en la red Wi-Fi)`);
     console.log('==============================================');
 });
